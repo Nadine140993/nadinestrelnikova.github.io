bd2aabda66d9354535cbc4d5ddf569c3f496f51f
Nadine140993 <61985982+Nadine140993@users.noreply.github.com>
2020-03-16 13:23:35 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 function resume() {
     var content = document.getElementById("content");
-    if (content.style.display === "none") {
-      content.style.display = "block";
-      document.getElementById("resume").innerHTML="Hide";
-    } else {
+    if (content.style.display === "block") {
       content.style.display = "none";
       document.getElementById("resume").innerHTML="View CV";
+    } else {
+      content.style.display = "block";
+      document.getElementById("resume").innerHTML="Hide";
     }
   }
